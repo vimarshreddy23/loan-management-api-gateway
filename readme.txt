f823d5d64c85b97871d85fcc9c1b656d134c273e
Reddy,Vimarsha <Vimarsha.Reddy@cognizant.com>
2020-09-04 15:18:07 +0530

    api-gatewat-changes

---
+++ readme.txt
@@ -0,0 +1,5 @@
+http://localhost:8765/login-service/api/login
+{
+    "userName":"TEST1",    
+    "userPassword":"TEST"
+}
---
+++ src/main/java/com/loan/managment/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.loan.managment.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+}
